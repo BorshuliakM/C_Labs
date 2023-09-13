a7e1663d72cfdc007ed6729db4786c6b13e60da3
BorshuliakM <borshuliakm@gmail.com>
2023-09-13 12:18:17 +0300

    Done

--- Task_9.c
+++ Task_9.c
@@ -3,16 +3,13 @@
 
 int main()
 {
-    int x=1, 
-        y=3, 
-        z=6;
-    float arithmeticMeanAbs, 
-          arithmeticMeanCube;
+    int x=1, y=3, z=6;
+    float arithmeticMeanAbs, arithmeticMeanCube;
 
     arithmeticMeanAbs=(abs(x)+abs(y)+abs(z))/3.0;
     arithmeticMeanCube=(pow(x,3)+pow(y,3)+pow(z,3))/3.0;
 
-    printf("%f\n", arithmeticMeanAbs);
-    printf("%f\n",arithmeticMeanCube);
+    printf("\nСума середнього арифметичного модулів %f", arithmeticMeanAbs);
+    printf("\nСума середнього арифметичного кубів %f", arithmeticMeanCube);
     return 0;
 }
