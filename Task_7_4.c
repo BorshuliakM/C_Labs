163da5837fdb4f9a5adb639ea3793c03dcdbc30b
BorshuliakM <borshuliakm@gmail.com>
2023-11-03 20:16:22 +0200

    .

--- Task_7_4.c
+++ Task_7_4.c
@@ -1,57 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int fact(int n)
-{
-    int res = (n == 0) ? 1 : (n * fact(n - 1));
-    return res;
-}
 
 int main()
 {
-    int n;
-    double a, res;
-    printf("Введіть число n = ");
+     int n;
+    printf("Введіть натуральне число n: ");
     scanf("%d", &n);
-    for (int i = 1; i <= n; i++)
-    {
-        a += 1.1;
-        res += a / fact(i);
+
+    // Перевірка на коректність n
+    if (n <= 0) {
+        printf("n повинно бути натуральним числом.\n");
+        return 1;
     }
 
-    printf("Сумма виразу = %.4lf", res);
+    double x;
+    printf("Введіть число x: ");
+    scanf("%lf", &x);
+
+    double a[n];
+    printf("Введіть %d дійсних чисел a1, a2, ..., an:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%lf", &a[i]);
+    }
+
+    // Підрахунок кількості разів, коли x зустрічається серед чисел a1, a2, ..., an
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        double sum = 0;
+        for (int j = 0; j <= i; j++) {
+            sum += a[j];
+        }
+        if (sum == x) {
+            count++;
+        }
+    }
+
+    printf("Число x зустрічається серед чисел a1, a2, ..., an %d разів.\n", count);
 
     return 0;
 }
 
-/*## Програмування
-# Лабораторна робота № 4
-# Тема
-Цикли
-# Виконавець
-Студент групи KNms1-B23 Боршуляк М.О.
 
-# Банк питань P BANK 03-04
-# Завдання 7
-Текст завдання
-7. Дано натуральне число
-n
-і дійсні числа
-a a an
-, ,..., 1 2
-. Обчислити:
-!
-...
-1! 2!
-1 2
-n
-a a an    .
-# Код знаходиться в репозиторії:
-https://github.com/BorshuliakM/C_Labs/tree/Task_11_4
-
-# Ім'я файлу (файлів)
-Task_7_4.c
-
-# Результат виконання програми
-Сумма виразу = 2.9901
-*/
